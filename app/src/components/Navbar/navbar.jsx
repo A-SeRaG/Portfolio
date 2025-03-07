36e814d7e065cde6a78777b2ea6ab02d20f62abb
SeRaG <aserag0101.com>
2025-03-08 00:58:33 +0200

    second

---
+++ app/src/components/Navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo'
+import links from './links'
+import btn from './btn'
+
+const navbar = () => {
+  return (
+    <nav>
+      <logo />
+      <links />
+      <btn />
+    </nav>
+  )
+}
+
+export default navbar
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  theme: {
+    fontFamily: {
+      body: ['Josefin Sans'],
+      special: ["Roboto"],
+    },
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
